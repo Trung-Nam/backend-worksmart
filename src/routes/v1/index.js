60c7d71f5a083e55ef38719def37cd1aec48095c
Trung Nam <trungnam.work@gmail.com>
2024-10-25 09:53:43 +0700

    Setup express router and http-status-codes lib

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,3 +1,12 @@
-/**
- *
- */
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { boardRouters } from './boardRoutes';
+
+const Router = express.Router();
+
+Router.get('/status', (req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' });
+});
+
+Router.use('/boards', boardRouters)
+export const APIs_V1 = Router;

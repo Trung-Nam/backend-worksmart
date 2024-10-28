d9091208c684149f1187f7116ca3d1be8f3c491d
Trung Nam <trungnam.work@gmail.com>
2024-10-28 23:43:02 +0700

    Update Whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tài nguyên
 export const WHITELIST_DOMAINS = [
     // 'http://localhost:5173' //Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev
+    'frontend-worksmart.vercel.app'
 ]
 
 export const BOARD_TYPE = {
